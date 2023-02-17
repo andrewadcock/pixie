57e0bd2e161dc741e649683dd14e57e7b1c98fc8
Andrew Adcock <andrewadcock@gmail.com>
2023-02-17 06:47:58 -0500

    Add prettier and reformat Layout.tsx

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,18 +1,18 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-interface Props {
-    children: JSX.Element;
-}
-export default function Layout(props: Props) {
-    const {children} = props;
 
-    return (
-        <div>
-            <Header />
-                <main>
-                    {children}
-                </main>
-            <Footer />
-        </div>
-    )
+interface Props {
+  children: JSX.Element;
+}
+
+export default function Layout(props: Props) {
+  const { children } = props;
+
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 }
