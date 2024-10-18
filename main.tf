54252d29e95c2132187f335b6c39a4ec7e89ab88
Andrew Adcock <andrew@4mile.io>
2024-10-18 09:41:02 -0400

    Add terraform support

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "google_cloudbuild_trigger" "react-trigger" {
+  //Source section
+  trigger_template {
+    branch_name = "main"
+    repo_name   = "pixie"
+  }
+
+  ignored_files = [".gitignore"]
+//Configuration section
+ // build config file
+ filename = "<path to cloudbuild.yaml file>"
+ // build config inline yaml
+ #build {
+ #    step {
+ #    name       = "node" 
+ #    entrypoint = "npm"
+ #    args       = ["install"]
+ #    }
+ #    step{...}
+ #    ...
+ #  }
+  //Advanced section
+  substitutions = {
+    <key1>= "<value1>"
+    <key2> = "<value2>"
+  }
+}
