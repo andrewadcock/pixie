3f4bb74c159634f894285d92b2663ee53c582bfa
Andrew Adcock <andrew@4mile.io>
2024-10-28 08:07:45 -0400

    Update docker for GCP cloudrun

--- main.tf
+++ main.tf
@@ -10,18 +10,43 @@ resource "google_cloudbuild_trigger" "react-trigger" {
  // build config file
  filename = "./cloudbuild.yaml"
  // build config inline yaml
- #build {
- #    step {
- #    name       = "node" 
- #    entrypoint = "npm"
- #    args       = ["install"]
- #    }
- #    step{...}
- #    ...
- #  }
+ build {
+    step {
+        name       = "node" 
+        entrypoint = "npm"
+        args       = ["install"]
+    }
+    step {
+        name        = "node"
+        entrypoint  = "npm"
+        args        = ["run"] 
+    }
+  }
   //Advanced section
 #   substitutions = {
 #     <key1>= "<value1>"
 #     <key2> = "<value2>"
 #   }
+}
+
+resource "google_artifact_registry_repository" "pixie-438313" {
+  location      = "us-central1"
+  repository_id = "pixie-frontend"
+  description   = "frontend docker image"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
+resource "google_artifact_registry_repository" "pixie-438313" {
+  location      = "us-central1"
+  repository_id = "pixie-backend"
+  description   = "backend docker image"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
 }
