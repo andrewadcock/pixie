4bf71a0a6a1605b09635913f4ed9f90807a1f659
Andrew Adcock <andrew@4mile.io>
2024-10-29 08:07:02 -0400

    Tweak for GCP

--- backend-config.tf
+++ backend-config.tf
@@ -1,18 +1,18 @@
-terraform {
-  backend "gcs" {
-    bucket  = var.bucket
-    prefix  = "state"
-  }  
-  required_version = ">= 0.12.7"  
-  required_providers { 
-    google = {
-      source = "hashicorp/google"
-      version = "3.82.0"
-    }
-  }
-}
-provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-}
+# terraform {
+#   backend "gcs" {
+#     bucket  = var.bucket
+#     prefix  = "state"
+#   }  
+#   required_version = ">= 0.12.7"  
+#   required_providers { 
+#     google = {
+#       source = "hashicorp/google"
+#       version = "3.82.0"
+#     }
+#   }
+# }
+# provider "google" {
+#   project = var.project
+#   region  = var.region
+#   zone    = var.zone
+# }
--- main.tf
+++ main.tf
@@ -1,52 +1,52 @@
-resource "google_cloudbuild_trigger" "react-trigger" {
-  //Source section
-  trigger_template {
-    branch_name = "main"
-    repo_name   = "pixie"
-  }
-
-  ignored_files = [".gitignore"]
-//Configuration section
- // build config file
- filename = "./cloudbuild.yaml"
- // build config inline yaml
- build {
-    step {
-        name       = "node" 
-        entrypoint = "npm"
-        args       = ["install"]
-    }
-    step {
-        name        = "node"
-        entrypoint  = "npm"
-        args        = ["run"] 
-    }
-  }
-  //Advanced section
-#   substitutions = {
-#     <key1>= "<value1>"
-#     <key2> = "<value2>"
+# resource "google_cloudbuild_trigger" "react-trigger" {
+#   //Source section
+#   trigger_template {
+#     branch_name = "main"
+#     repo_name   = "pixie"
 #   }
-}
 
-resource "google_artifact_registry_repository" "pixie-438313" {
-  location      = "us-central1"
-  repository_id = "pixie-frontend"
-  description   = "frontend docker image"
-  format        = "DOCKER"
+#   ignored_files = [".gitignore"]
+# //Configuration section
+#  // build config file
+#  filename = "./cloudbuild.yaml"
+#  // build config inline yaml
+#  build {
+#     step {
+#         name       = "node" 
+#         entrypoint = "npm"
+#         args       = ["install"]
+#     }
+#     step {
+#         name        = "node"
+#         entrypoint  = "npm"
+#         args        = ["run"] 
+#     }
+#   }
+#   //Advanced section
+# #   substitutions = {
+# #     <key1>= "<value1>"
+# #     <key2> = "<value2>"
+# #   }
+# }
 
-  docker_config {
-    immutable_tags = true
-  }
-}
+# resource "google_artifact_registry_repository" "pixie-438313" {
+#   location      = "us-central1"
+#   repository_id = "pixie-frontend"
+#   description   = "frontend docker image"
+#   format        = "DOCKER"
 
-resource "google_artifact_registry_repository" "pixie-438313" {
-  location      = "us-central1"
-  repository_id = "pixie-backend"
-  description   = "backend docker image"
-  format        = "DOCKER"
+#   docker_config {
+#     immutable_tags = true
+#   }
+# }
 
-  docker_config {
-    immutable_tags = true
-  }
-}
+# resource "google_artifact_registry_repository" "pixie-438313" {
+#   location      = "us-central1"
+#   repository_id = "pixie-backend"
+#   description   = "backend docker image"
+#   format        = "DOCKER"
+
+#   docker_config {
+#     immutable_tags = true
+#   }
+# }
